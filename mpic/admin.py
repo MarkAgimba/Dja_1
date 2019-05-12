b0d6ee4235a252a473d108e01e1e96008a3314f3
Mark Agimba <markaguero16@gmail.com>
2019-05-12 11:24:41 +0300

    add class ImgAdmin

--- mpic/admin.py
+++ mpic/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from .models import Location, Image, categories
 
 # Register your models here.
+ 
+ class ImgAdmin(admin.ModelAdmin):
+     filter_horizontal = ('categories',)
+
+admin.site.register(Location)
+admin.site.register(Image,ImgAdmin)
+admin.site.register(categories)
